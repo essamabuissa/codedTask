94f5bbe70d9b1bf301cb498119bdc2af87c72368
Dinaisbaih <isbaihdina.f@outlook.com>
2021-10-22 15:01:43 +0300

    Added apollo setup and navigation container and linking config

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { NavigationContainer } from "@react-navigation/native";
+import RootStackNavigation from "./src/Navigation";
+import * as Linking from "expo-linking";
 
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+const prefix = Linking.createURL("/");
+const config = {
+  screens: {
+    CharacterStack: {
+      screens: {
+        Characters: "characters",
+        CharacterDetails: {
+          path: "characters/:id",
+        },
+      },
+    },
+  },
+};
 export default function App() {
+  const linking = {
+    prefixes: [prefix],
+    config,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <NavigationContainer linking={linking}>
+        <RootStackNavigation />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
