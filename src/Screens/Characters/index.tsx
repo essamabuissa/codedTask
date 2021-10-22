9db8345321f9724b491e4f3e34a20dbd75fc10de
Dinaisbaih <isbaihdina.f@outlook.com>
2021-10-22 15:05:56 +0300

    Added CharactersList

---
+++ src/Screens/Characters/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  ListRenderItem,
+  Image,
+} from "react-native";
+import CharacterCard from "../../Components/CharacterCard/CharacterCard";
+import { FETCH_CHARACTERS } from "../../GraphQL/CharactersGraphQl";
+import { CharacterInterface } from "../../Types";
+import { styles } from "./styles";
+import { useLinkTo } from "@react-navigation/native";
+
+const CharactersList = () => {
+  const { data } = useQuery(FETCH_CHARACTERS);
+  const characters = data?.characters?.results;
+  const linkTo = useLinkTo();
+  const navigateToDetails = (item: CharacterInterface) => {
+    linkTo(`/characters/${item?.id}`);
+  };
+  //   /${item?.status}/${item?.gender}/${item?.species}
+  const renderItem: ListRenderItem<CharacterInterface> | null | undefined = ({
+    item,
+  }) => {
+    return (
+      <CharacterCard
+        onPress={() => navigateToDetails(item)}
+        to={{ screen: "CharacterDetails", params: { character: item } }}
+        character={item}
+      />
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <SafeAreaView />
+      <Image
+        source={{
+          uri: "https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
+        }}
+        style={styles.logo}
+      />
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={characters}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        onEndReachedThreshold={0.5}
+      />
+    </View>
+  );
+};
+
+export default CharactersList;
---
+++ src/Screens/Characters/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  list: {
+    marginTop: 10,
+  },
+  logo: {
+    width: "100%",
+    height: "20%",
+    alignSelf: "center",
+    borderRadius: 10,
+  },
+});
