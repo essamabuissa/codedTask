b3171d841c077d4b2125ed29b58f08312ac4e1f0
Dinaisbaih <isbaihdina.f@outlook.com>
2021-10-22 15:09:20 +0300

    Added loading to home page

--- src/Screens/Characters/index.tsx
+++ src/Screens/Characters/index.tsx
@@ -7,6 +7,7 @@ import {
   FlatList,
   ListRenderItem,
   Image,
+  ActivityIndicator,
 } from "react-native";
 import CharacterCard from "../../Components/CharacterCard/CharacterCard";
 import { FETCH_CHARACTERS } from "../../GraphQL/CharactersGraphQl";
@@ -15,7 +16,7 @@ import { styles } from "./styles";
 import { useLinkTo } from "@react-navigation/native";
 
 const CharactersList = () => {
-  const { data } = useQuery(FETCH_CHARACTERS);
+  const { loading, data } = useQuery(FETCH_CHARACTERS);
   const characters = data?.characters?.results;
   const linkTo = useLinkTo();
   const navigateToDetails = (item: CharacterInterface) => {
@@ -34,22 +35,32 @@ const CharactersList = () => {
     );
   };
   return (
-    <View style={styles.container}>
-      <SafeAreaView />
-      <Image
-        source={{
-          uri: "https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
-        }}
-        style={styles.logo}
-      />
-      <FlatList
-        contentContainerStyle={styles.list}
-        data={characters}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        onEndReachedThreshold={0.5}
-      />
-    </View>
+    <>
+      {loading ? (
+        <ActivityIndicator
+          size="small"
+          color="black"
+          style={{ marginTop: 50 }}
+        />
+      ) : (
+        <View style={styles.container}>
+          <SafeAreaView />
+          <Image
+            source={{
+              uri: "https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
+            }}
+            style={styles.logo}
+          />
+          <FlatList
+            contentContainerStyle={styles.list}
+            data={characters}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            onEndReachedThreshold={0.5}
+          />
+        </View>
+      )}
+    </>
   );
 };
 
